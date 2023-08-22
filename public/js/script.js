59af8b10891cf450ee13f5b4343043f48fd95386
Zenn1104 <basoarif34@gmail.com>
2023-08-22 18:19:09 +0700

    feat(index) : added feature back to top

--- public/js/script.js
+++ public/js/script.js
@@ -3,12 +3,17 @@
 window.onscroll = function() {
  const header = document.querySelector('header');
  const fixedNav = header.offsetTop;
+ const toTop = document.querySelector('#to-top')
 
 
  if(window.scrollY > fixedNav) {
     header.classList.add('navbar-fixed')
+    toTop.classList.remove('hidden')
+    toTop.classList.add('flex')
  }else{
     header.classList.remove('navbar-fixed')
+   toTop.classList.remove('flex')
+    toTop.classList.add('hidden')
  }
 };
 
@@ -23,3 +28,13 @@ hamburger.addEventListener('click', function() {
     hamburger.classList.toggle('hamburger-active');
     navMenu.classList.toggle('hidden');
 });
+
+
+// click di luar hamburger
+
+// window.addEventListener('click' , function(e) {
+//    if(e.target != hamburger && e.target != navMenu) {
+//       navMenu.classList.remove('hamburger-active')
+//       navMenu.classList.add('hidden')
+//    }
+// })
